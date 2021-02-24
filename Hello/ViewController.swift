a3992538144892616f0348af4e96715cd1bfacdc
Heri Susanto <herisusanto.id@gmail.com>
2021-02-24 12:06:57 +0700

    Learning basic UIKit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Heri Susanto on 24/02/21.
+//  Copyright © 2021 Heri Susanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
